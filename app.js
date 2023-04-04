1b4dd397f2a96ffe7558a319e9e72fd05504c33b
goosefin <agustinafin96@gmail.com>
2023-04-03 21:57:32 -0400

    Name, height, abilities and moves are rendering on screen

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 let pokeName = document.querySelector('#poke-name')
 let search = document.querySelector('#search')
 let pokeInfo = document.querySelector('#poke-info')
+let pokeHeight = document.querySelector('#height')
+let pokeAbilities = document.querySelector('#abilities')
+let pokeMoves = document.querySelector('#moves')
 
 const abilities = (pokemon) => {
     for(let skill of pokemon.abilities){
@@ -9,20 +12,35 @@ const abilities = (pokemon) => {
 }
 
 let check = () => {
-    //console.log(pokeName.value)
-    //console.log('https://pokeapi.co/api/v2/pokemon/'+pokeName.value+'/')
     fetch('https://pokeapi.co/api/v2/pokemon/'+pokeName.value+'/')
         .then((response) => response.json())
         .then((data) => {
-            for(let skill of data.abilities){
-                console.log(skill.ability.name)
+            let abilitiesLimit = 3
+            for(let ability of data.abilities){
+                abilitiesLimit -= 1
+                pokeAbilities.innerText += ' ' + ability.ability.name + ', '
+                if(abilitiesLimit === 0){
+                    break
+                }
             }
-            console.log(data.height)
+            pokeHeight.innerText = 'Height: ' + data.height 
+            let movesLimit = 3
             for(let move of data.moves){
-                console.log(move.move.name)
+                movesLimit -= 1
+                pokeMoves.innerText += ' ' + move.move.name + ', '
+                if(movesLimit === 0){
+                    break
+                }
             }
             pokeInfo.innerText = data.name
         })
+}
+
+let list = ['a','b','c']
+let string =''
+for(let val of list){
+    string += val + ' '
+    //console.log(string)
 }
 
 search.addEventListener('click',check)
