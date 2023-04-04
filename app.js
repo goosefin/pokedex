362cc164655c5a95c96420eea74b14246f0c8b7e
goosefin <agustinafin96@gmail.com>
2023-04-03 21:19:50 -0400

    looping through pokemon and logging info

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ let pokeName = document.querySelector('#poke-name')
 let search = document.querySelector('#search')
 let pokeInfo = document.querySelector('#poke-info')
 
+const abilities = (pokemon) => {
+    for(let skill of pokemon.abilities){
+        console.log(skill.ability.name)
+    }
+}
+
 let check = () => {
     //console.log(pokeName.value)
     //console.log('https://pokeapi.co/api/v2/pokemon/'+pokeName.value+'/')
     fetch('https://pokeapi.co/api/v2/pokemon/'+pokeName.value+'/')
         .then((response) => response.json())
         .then((data) => {
-            pokeInfo.innerText = data.id
+            for(let skill of data.abilities){
+                console.log(skill.ability.name)
+            }
+            console.log(data.height)
+            for(let move of data.moves){
+                console.log(move.move.name)
+            }
+            pokeInfo.innerText = data.name
         })
 }
 
